b949d8d9816cf614fb42bced8581cb9ada357b62
Md Nuruddin <nuruddinalways99@gmail.com>
2023-01-31 13:03:09 +0600

    Footer bottom

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+    <footer></footer>
+    <footer></footer>
+    <footer className='py-3'>
+      <div className="container-xxl">
+        <div className="row">
+          <div className="col-12">
+            <p className='text-center text-white mb-0 font-italic'>&copy;2023 Powered by itznur07</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    </>
   )
 }
 
