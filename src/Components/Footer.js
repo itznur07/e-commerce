b90e1cacae6ad35955b4eac0cc3170595d1dd401
Md Nuruddin <nuruddinalways99@gmail.com>
2023-01-31 17:34:58 +0600

    Footer top

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,9 +3,34 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
+    <footer className='py-5'>
+      <div className="container-xxl">
+        <div className="row">
+          <div className="col-5">
+            <div className="footer-top-data d-flex align-items-center gap-30">
+              <img src="/images/newsletter.png" alt="newsletter" />
+              <h3 className='mb-0 text-white'>Subscribe for a newsletter</h3>
+            </div>
+          </div>
+          <div className="col-7">
+          <div class='input-group'>
+                <input
+                  type='text'
+                  class='form-control p-1'
+                  placeholder='youremail@domain.com'
+                  aria-label='youremail@domain.com'
+                  aria-describedby='basic-addon2'
+                />
+                <span class='input-group-text p-2 bg-dark text-white border-0' id='basic-addon2'>
+                  Subscribe
+                </span>
+              </div>
+          </div>
+        </div>
+      </div>
+    </footer>
     <footer></footer>
-    <footer></footer>
-    <footer className='py-3'>
+    <footer className='py-3 pt-5 '>
       <div className="container-xxl">
         <div className="row">
           <div className="col-12">
