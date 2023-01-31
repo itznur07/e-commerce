f86654f0a46f51c4a259fedf1840d337e3c20555
Md Nuruddin <nuruddinalways99@gmail.com>
2023-01-31 10:05:23 +0600

    Header top strip

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,9 +2,22 @@ import React from 'react'
 
 const Header = () => {
   return (
-   <div>
-    Header
-   </div>
+   <>
+    <div className="header-top-strip py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <p className='text-white mb-0'>Free shiping over 100$ and free return</p>
+            </div>
+            <div className="col-6">
+              <p className="text-end text-white mb-0">
+                Hotline: <a className='text-white' href="h">+9035 4345 3433</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+   </>
   )
 }
 
